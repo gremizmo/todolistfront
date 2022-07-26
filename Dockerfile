fabd2d8e3f3728d5f58b503196be8eb5944bee4d
Matthieu <mgremaud@leadersleague.com>
2022-07-26 17:40:45 +0200

    feat(login): add login view

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.14.0
+FROM node:16.10.0
 
 WORKDIR .
 
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Header />
+  <div class="container p-5">
+    <h3 class="text-center mt-2 mb-5">showOffList.com</h3>
+    <div class="col-md-12">
+
+      <form v-on:submit.prevent="login_user">
+
+        <div class="mb-3">
+          <label for="exampleFormControlInput2" class="form-label">Enter Email</label>
+          <input type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email" v-model="form.email">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput3" class="form-label">Enter Password</label>
+          <input type="password" name="password" class="form-control" id="exampleInputPass1" aria-describedby="passHelp" placeholder="Enter Password" v-model="form.password">
+        </div>
+
+
+        <button type="submit" class="btn btn-primary mt-5">Submit</button>
+      </form>
+    </div>
+  </div>
+  <Footer />
 </template>
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios'
+import Swal from 'sweetalert2'
+export default {
+  components: {
+  },
+  data(){
+    return {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+      form:{
+        email: '',
+        password: ''
 
-nav {
-  padding: 30px;
-}
+      }
+    }
+  },
+  methods:{
+    //user login function and api call
+    login_user(){
+      axios
+          .post('http://localhost:8000/api/v1/auth/login',this.form)
+          .then((resp) =>{
+            this.form.email = '';
+            this.form.password = '';
+            if(resp["data"]["status"] === "error")
+            {
+              Swal.fire({
+                title: 'OPPS',
+                text:   "error",
+                icon: 'warning',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+              });
+            }
+            else
+            {
+              Swal.fire({
+                title: 'Hurry',
+                text:   "You have been logged-in successfully",
+                icon: 'success',
 
-nav a.router-link-exact-active {
-  color: #42b983;
+              });
+            }
+
+          })
+          .catch((e)=>{
+            console.log(e);
+            Swal.fire({
+              title: 'Hurry',
+              text:   e,
+              icon: 'warning',
+
+            });
+          })
+    }
+  }
 }
-</style>
+</script>
